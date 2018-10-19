fbde96b031032f8e5b0a7a23b849a9e1448e8373
BandsWithLegends <adamosimpson21@gmail.com>
2018-10-19 01:22:34 -0700

    Enabling cors for client site3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const errorHandler = require("./handlers/error")
 
 //handling CORS
 const corsOptions = {
-  origin: 'https://wepoll.herokuapp.com',
+  origin: 'https://wepol.herokuapp.com',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
