f0148f3fc67c6dce1e95fdb7efdd3e5a9cb3a97e
BandsWithLegends <adamosimpson21@gmail.com>
2019-01-23 01:17:40 -0800

    Refactoring party handlers to use cleaner syntax

--- handlers/party.js
+++ handlers/party.js
@@ -2,52 +2,32 @@ const db = require('../models');
 
 exports.getParties = function(req, res){
   db.Party.find()
-    .then(parties => {
-      res.json(parties)
-    })
-    .catch(err => {
-      res.send(err)
-    })
+    .then(parties => res.json(parties))
+    .catch(err => res.send(err))
 }
 
 exports.createParty = function(req, res){
   db.Party.create(req.body)
-    .then(newParty => {
-      res.status(201).json(newParty);
-    })
-    .catch(err => {
-      res.send(err);
-    })
+    .then(newParty => res.status(201).json(newParty))
+    .catch(err => res.send(err))
 }
 
 exports.getParty = function(req, res){
   db.Party.findById(req.params.partyId)
-    .then(foundParty => {
-      res.json(foundParty)
-    })
-    .catch(err => {
-      res.send(err);
-    })
+    .then(foundParty => res.json(foundParty))
+    .catch(err => res.send(err))
 }
 
 exports.updateParty = function(req, res){
   db.Party.findOneAndUpdate({_id:req.params.partyId}, req.body, {new: true})
-    .then(party => {
-      res.json(party)
-    })
-    .catch(err => {
-      res.send(err);
-    })
+    .then(party => res.json(party))
+    .catch(err => res.send(err))
 }
 
 exports.deleteParty = function(req, res){
   db.Party.deleteOne({_id:req.params.partyId})
-    .then(() => {
-      res.json({message:"Deleted Party"})
-    })
-    .catch(err => {
-      res.send(err);
-    })
+    .then(() => res.json({message:"Deleted Party"}))
+    .catch(err => res.send(err))
 }
 
 exports.joinParty = async function(req, res, next){
